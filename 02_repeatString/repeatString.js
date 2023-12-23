7a94e8d07600b1e896a1a606ada4296a7901a969
Fred <leferpz@gmail.com>
2023-12-23 16:30:45 -0500

    Finished 2nd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
+const repeatString = function(word, number) {
+    let newString = "";
+    
+    if (number === 0) {
+        return ""
+    }
+    
+    if (number < 0) {
+        return "ERROR";
+    }
 
+    let i = 1;
+    while (i <= number) {
+        newString += word;
+        i++;
+    }
+    return newString;
 };
+
+console.log(repeatString("yeh", 2))
 
 // Do not edit below this line
 module.exports = repeatString;
