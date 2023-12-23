69b4017f972594cb84fae7550d1088ae4c72b13f
Fred <leferpz@gmail.com>
2023-12-23 16:40:49 -0500

    Completed exercise 3 (reverse string function)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splitString = string.split('');
+    let finalString = ""
+    let i = 1;
 
+    while (i <= splitString.length) {
+        finalString += splitString[splitString.length - i];
+        i++;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
