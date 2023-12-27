df7dae3786a66afb1ba61d8e7689aacd744fe5bc
Fred <leferpz@gmail.com>
2023-12-27 14:54:53 -0500

    Finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let count = 0;
+    let list = [];
+    for (let x = num1; x <= num2; x++) {
+        list.push(x);
+    }
+    console.log(list);
+    for (let y = 0; y < list.length; y++) {
+        count += list[y];
+    }
+    console.log(count);
+    return count
 };
 
+sumAll(10,40)
+
 // Do not edit below this line
-module.exports = sumAll;
+// module.exports = sumAll;
+
+// Function must take 2 integers x
+// Initialise a counter variable x
+// Generate an array of all the numbers between them
+// Iterate through array and add each number to the counter
+// Return counter
