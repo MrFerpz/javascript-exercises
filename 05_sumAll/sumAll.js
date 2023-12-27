18e525867e9c5aaf11ca7669413b55474b97bc5e
Fred <leferpz@gmail.com>
2023-12-27 16:06:42 -0500

    Retrospectively fixed all the tests because I forgot to save the files. Re-did task 5 with sorted numbers approach.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,21 +1,26 @@
 const sumAll = function(num1, num2) {
     let count = 0;
+    let sortedNumbers = [num1, num2].sort();
     let list = [];
-    for (let x = num1; x <= num2; x++) {
-        list.push(x);
+if ((num1 >= 0 && num2 >= 0) && (Number.isInteger(num1) && Number.isInteger(num2))) {
+        for (let x = sortedNumbers[0]; x <= sortedNumbers[1]; x++) {
+            list.push(x);
+        }
+        console.log(list);
+        for (let y = 0; y < list.length; y++) {
+            count += list[y];
+        }
+        console.log(count);
+        return count
+    } else {
+        return "ERROR"
     }
-    console.log(list);
-    for (let y = 0; y < list.length; y++) {
-        count += list[y];
-    }
-    console.log(count);
-    return count
 };
 
 sumAll(10,40)
 
 // Do not edit below this line
-// module.exports = sumAll;
+module.exports = sumAll;
 
 // Function must take 2 integers x
 // Initialise a counter variable x
