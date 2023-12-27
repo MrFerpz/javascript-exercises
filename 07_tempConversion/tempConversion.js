c521b57de9c21d73845d8b1371c08c222848fc39
Fred <leferpz@gmail.com>
2023-12-27 15:53:31 -0500

    Exercise 7 completed.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  let celsiusNumber = (number - 32) * (5/9);
+  let roundedCelsius = Math.round(celsiusNumber * 10) / 10;
+  return roundedCelsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  let fahrenheitNumber = (number * 9/5) + 32;
+  let roundedFahrenheit = Math.round(fahrenheitNumber * 10) / 10;
+  return roundedFahrenheit
 };
 
 // Do not edit below this line
