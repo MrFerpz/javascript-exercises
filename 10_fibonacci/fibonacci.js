77447a2afd720345b2a46392e6c7f2e377b3925a
Fred <leferpz@gmail.com>
2024-01-06 16:48:43 -0500

    Exercise 10 completed.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let newNumber = Number(number);
+    
+    if (newNumber === 0) {
+        return 0
+    }
 
+    if (newNumber < 0) {
+        return "OOPS"
+    }
+
+    let newArray = [1, 1];
+    for (let i = 2; i <= 100; i++) {
+        newArray[i] = (newArray[i-2] + newArray[i-1])
+    }
+    return newArray[newNumber - 1];
 };
 
 // Do not edit below this line
