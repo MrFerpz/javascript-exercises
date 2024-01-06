6f46a9b43296415d04f9468ea6ee66c42b5b3f09
Fred <leferpz@gmail.com>
2024-01-06 16:24:48 -0500

    Exercise 9 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let newString = string.replaceAll(/[.,?! ]/g, "");
+    let upperCaseString = newString.toUpperCase();
+    let array = Array.from(upperCaseString);
+    console.log(array);
 
-};
-
+    for (let i = 0; i < array.length / 2; i++) {
+        if (array[i] !== array[(array.length -1 -i)]) {
+            return false;
+        }
+    }
+    return true;
+}
 // Do not edit below this line
 module.exports = palindromes;
