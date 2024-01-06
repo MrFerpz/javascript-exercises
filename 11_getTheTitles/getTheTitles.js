4113e9ce2543b1fd000e4d85f7b2b96caaafd869
Fred <leferpz@gmail.com>
2024-01-06 17:24:33 -0500

    Exercise 11 complete.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(bookslist) {
+    newArray = [];
+    for (let i = 0; i < bookslist.length; i++) {
+        newArray.push(bookslist[i].title);
+    }
+    return newArray
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
